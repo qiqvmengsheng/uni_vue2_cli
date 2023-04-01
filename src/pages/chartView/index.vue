607a63e6ea88aeb14087095ff84806eff0dbc212
luoziqi <luoziqi@sarad-china.com>
2023-04-01 17:58:46 +0800

    引入echart组件，测试echart

--- src/pages/chartView/index.vue
+++ src/pages/chartView/index.vue
@@ -1,17 +1,154 @@
 <template>
-  <div class="chartView">chartView</div>
+  <div class="chartView">
+    <!-- <view class="chart">
+      <mpvue-echarts
+        :echarts="echarts"
+        :onInit="onInit"
+        ref="chart"
+        canvasId="chart-canvas"
+      />
+    </view> -->
+    <view class="chart">
+      <l-echart ref="chart" @finished="init"></l-echart>
+    </view>
+  </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 import to from 'await-to-js';
 import { toast, confirm } from '@uni/apis';
-import { measuredGetdata } from '@/api/devdata';
+import LEchart from '@/components/l-echart/l-echart';
+import { measuredGetdata, getlastDatas } from '@/api/devdata';
+
+// import * as echarts from 'echarts';
+// 按需引入 开始
+import * as echarts from 'echarts/core';
+import { LineChart, BarChart } from 'echarts/charts';
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LegendComponent,
+} from 'echarts/components';
+// 标签自动布局，全局过渡动画等特性
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+// 引入 Canvas 渲染器，注意引入 CanvasRenderer 是必须的一步
+import { CanvasRenderer } from 'echarts/renderers';
+// 注册必须的组件
+echarts.use([
+  LegendComponent,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LineChart,
+  BarChart,
+  LabelLayout,
+  UniversalTransition,
+  CanvasRenderer,
+]);
+// -------------按需引入结束------------------------
 
 export default {
-  components: {},
+  components: { LEchart },
   data() {
-    return {};
+    return {
+      option: {
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow',
+          },
+          confine: true,
+        },
+        legend: {
+          data: ['热度', '正面', '负面'],
+        },
+        grid: {
+          left: 20,
+          right: 20,
+          bottom: 15,
+          top: 40,
+          containLabel: true,
+        },
+        xAxis: [
+          {
+            type: 'value',
+            axisLine: {
+              lineStyle: {
+                color: '#999999',
+              },
+            },
+            axisLabel: {
+              color: '#666666',
+            },
+          },
+        ],
+        yAxis: [
+          {
+            type: 'category',
+            axisTick: { show: false },
+            data: [
+              '汽车之家',
+              '今日头条',
+              '百度贴吧',
+              '一点资讯',
+              '微信',
+              '微博',
+              '知乎',
+            ],
+            axisLine: {
+              lineStyle: {
+                color: '#999999',
+              },
+            },
+            axisLabel: {
+              color: '#666666',
+            },
+          },
+        ],
+        series: [
+          {
+            name: '热度',
+            type: 'bar',
+            label: {
+              normal: {
+                show: true,
+                position: 'inside',
+              },
+            },
+            data: [300, 270, 340, 344, 300, 320, 310],
+          },
+          {
+            name: '正面',
+            type: 'bar',
+            stack: '总量',
+            label: {
+              normal: {
+                show: true,
+              },
+            },
+            data: [120, 102, 141, 174, 190, 250, 220],
+          },
+          {
+            name: '负面',
+            type: 'bar',
+            stack: '总量',
+            label: {
+              normal: {
+                show: true,
+                position: 'left',
+              },
+            },
+            data: [-20, -32, -21, -34, -90, -130, -110],
+          },
+        ],
+      },
+    };
   },
   computed: {
     ...mapGetters(['name', 'devices']),
@@ -21,9 +158,23 @@ export default {
       const id = this.$Route.query.deviceid;
       [this.dev] = this.devices.filter((dev) => dev.deviceid === id);
       console.log(this.dev);
+      this.getdata();
     });
   },
   methods: {
+    async getdata() {
+      const [err, res] = await to(
+        getlastDatas({
+          deviceId: this.dev.deviceid,
+          numbers: 20,
+        })
+      );
+      if (err) {
+        console.log(err, res);
+        return;
+      }
+      console.log(res);
+    },
     async getdevdata() {
       const [err, res] = await to(
         measuredGetdata({
@@ -36,6 +187,11 @@ export default {
         return;
       }
       console.log(res);
+    },
+    init() {
+      this.$refs.chart.init(echarts, (chart) => {
+        chart.setOption(this.option);
+      });
     },
   },
   watch: {},
@@ -61,4 +217,9 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.chart {
+  width: 730rpx;
+  height: 600rpx;
+}
+</style>
